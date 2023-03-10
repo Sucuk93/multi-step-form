895cd5d8d64b3b18efcdacce4833adc3d492f5a1
Sucuk93 <95504275+Sucuk93@users.noreply.github.com>
2023-03-10 10:45:01 +0100

    ohne Mobile View

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-marine": "var(--color-primary-marine)",
+        "primary-purplish": "var(--color-primary-purplish)",
+        "primary-pastel": "var(--color-primary-pastel)",
+        "primary-light": "var(--color-primary-light)",
+
+        "grey-cool": "var(--color-grey-cool)",
+        "grey-light": "var(--color-grey-light)",
+        "white-alabaster": "var(--color-white-alabaster)",
+        "white-magnolia": "var(--color-white-magnolia)",
+        "white-pure": "var(--color-white-pure)",
+      },
+    },
   },
   plugins: [],
 };
